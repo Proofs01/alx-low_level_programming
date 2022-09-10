181225e99d6594ac4d5e9f74d81a25634ee7164e
Isaiah Samson Ifeanyi <bigsamz4real49@yahoo.com>
2022-09-09 17:12:31 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -10,22 +9,26 @@ int main(void)
 {
 	int i:
 	int j;
+
 	for (i = 48; i < 58 i++)
 	{
-	for (j = 48; j < 58; j++)
-	{
-	if (i != j && i < j)
-	{
-putchar(i);
-putchar(j);
-	if (j == 57 && i == 56)
-	{
-break;
-	}
-putchar(',');
-	putchar(' ');
-	}
-	}
+	
+		for (j = 48; j < 58; j++)
+		{
+	
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (j == 57 && i == 56)
+				{
+					break
+						;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
