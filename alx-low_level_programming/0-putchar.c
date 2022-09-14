2bdc98d4f325e17ad751018d7b166393dc4fbd34
Isaiah Samson Ifeanyi <bigsamz4real49@yahoo.com>
2022-09-14 01:11:56 -0700

    0-putchar.c

--- alx-low_level_programming/0-putchar.c
+++ alx-low_level_programming/0-putchar.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
-
 /**
- * main - Entry point
+ * main - prints _putchar, followed by a new line
  *
- * Return: nothing, return void
+ * Return: 0
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
+ return (0);
 }
