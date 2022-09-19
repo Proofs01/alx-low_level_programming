8884b469e731aa94b2b13eaae7c2f676806cb959
Isaiah Samson Ifeanyi <bigsamz4real49@yahoo.com>
2022-09-19 14:50:39 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,9 +14,9 @@ char *_strcpy(char *dest, char *src)
 
 	for (index = 0;  src[index] != '\0'; index++)
 	{
-		dest [index] = src [index];
+		dest[index] = src[index];
 	}
-	dest [index] = '\0';
+	dest[index] = '\0';
 
 
 	return (dest);
