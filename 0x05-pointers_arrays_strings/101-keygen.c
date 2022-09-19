bd1f23b87bdd2b3290f405dbca14feff99d3dd7c
Isaiah Samson Ifeanyi <bigsamz4real49@yahoo.com>
2022-09-19 16:32:19 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -25,7 +25,7 @@ int main(void)
 
 	password[index] = '\0';
 
-	if (sum ! = 2772)
+	if (sum '!' = 2772)
 	{
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
@@ -34,7 +34,7 @@ int main(void)
 
 		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diff_hall2))
+			if (password[index] >= (33 + diff_half2))
 			{
 				password[index] -= diff__half2;
 			       	break;
