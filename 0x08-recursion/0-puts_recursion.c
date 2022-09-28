91d30255b990926bf72f3d5e82d54f42d4e4b70d
Isaiah Samson Ifeanyi <bigsamz4real49@yahoo.com>
2022-09-28 14:29:05 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Entry point
- * @s; input
- * Return: Always 0 (Success)
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
