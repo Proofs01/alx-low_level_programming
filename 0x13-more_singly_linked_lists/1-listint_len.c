142087354c1deea3e9fe3ec2a3d7a88d4480fc7a
Isaiah Samson Ifeanyi <bigsamz4real49@yahoo.com>
2022-10-25 15:51:34 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len - function that returns the number of elementsin a linked list
- * @h: pointer to the first element
- * Return: number of elements
+ * listint_len - Function that finds the number of elements in list
+ * @h: Pointer to the head of listin_len_t list
+ * Return: The number of elements in the list
  */
 
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t count = 0;/*number of nodes*/
 
-	while (h)
+	while (h != NULL)
 	{
-		h = h->next;
 		count++;
-	}
-
+		h = h->next;
+	}		
 	return (count);
 }
