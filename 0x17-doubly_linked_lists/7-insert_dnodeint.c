8bf891915a61b18f87f7138a70985c10daf486c9
Isaiah Samson Ifeanyi <bigsamz4real49@yahoo.com>
2022-12-08 12:23:22 -0800

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,40 +1,83 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - inserts a new node at specified index in D.L.L
- * @h: Address of pointer to head node
- * @idx: index at which to add the new node
- * @n: data to be added to the D.L.L
+ * linkedListLen - looks for lenght of a linked list
+ * @h: head of the linked list
  *
- * Return: address of new node (Success) or NULL (failed)
+ * Return: lenght of the list
+ */
+unsigned int linkedListLen(dlistint_t *h)
+{
+	dlistint_t *temp = h;
+	unsigned int len = 0;
+
+	if (h == NULL)
+		return (0);
+	while (temp)
+	{
+		temp = temp->next;
+		len++;
+	}
+	return (len);
+}
+/**
+ * createNode - creates a new node
+ * @n: new node data
+ *
+ * Return: new node
+ */
+dlistint_t *createNode(int n)
+{
+	dlistint_t *newNode = NULL;
+
+	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+
+	return (newNode);
+}
+/**
+ * insert_dnodeint_at_index - insert a new node at index
+ * @h: head of the list
+ * @idx: index to insert the new node
+ * @n: data of the new node
+ *
+ * Return: addres of the new element
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *curr, *new_node;
-	unsigned int i = 0;
+	dlistint_t *newNode = NULL, *temp = *h;
+	unsigned int listLen = linkedListLen(*h);
 
-	if (*h == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	curr = *h;
-	while (curr != NULL)
+	if (idx == 0)
 	{
-		if (i == idx)
-		{
-			(curr->prev)->next = new_node;
-			new_node->prev = curr->prev;
-			curr->prev = new_node;
-			new_node->next = curr;
-			return (new_node);
-		}
-		curr = curr->next;
-		i++;
+		temp = add_dnodeint(h, n);
+		return (temp);
 	}
-	free(new_node);
-	return (NULL);
+	if (idx > listLen)
+		return (NULL);
+	newNode = createNode(n);
+	if (idx == listLen)
+	{
+		while (temp->next)
+			temp = temp->next;
+		temp->next = newNode;
+		newNode->prev = temp;
+		return (newNode);
+	}
+	listLen = 0;
+	while (listLen != idx)
+	{
+		temp = temp->next;
+		listLen++;
+	}
+	newNode->next = temp;
+	newNode->prev = temp->prev;
+	(temp->prev)->next = newNode;
+	temp->prev = newNode;
+
+	return (newNode);
 }
